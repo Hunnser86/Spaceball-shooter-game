4bab3a79bcf93ac545fbb57cf50620e58ed9217f
Hunnser86 <robhunns@gmail.com>
2021-06-22 13:42:17 +0000

    Finished projectile object and added Math to allow projectile to travel where the canvas was clicked

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,6 +45,12 @@ class Projectile {
         ctxt.fillStyle = this.color   
         ctxt.fill()    
      }
+//Adding the x and y velocity with an update function 
+     update() {
+         this.draw()
+         this.x = this.x + this.velocity.x
+         this.y = this.y + this.velocity.y
+     }
 }
 
 const x = canvas.width / 2
@@ -55,10 +61,46 @@ const y = canvas.height / 2
 const player = new Player(x, y, 30, 'blue' )
 player.draw()
 
-console.log(player)
+//Put the projectile here to allow it be accessed within the animate function
+const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {
+   x: 1,
+   y: 1
+})
+
+//create an array to draw multiple projectiles
+const projectiles = []
+
+
+
+//Create a function to animate the projectile
+function animate() {
+    requestAnimationFrame(animate)
+    projectiles.forEach(projectile => {
+        projectile.update()
+    });
+    
+}
+
 
 //Add an event listener to listen for a click event which will create a projectile
-addEventListener('click', (event)=>{
-    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
-    projectile.draw()
+addEventListener('click', (event)=>
+    {
+//Trigonometry to make the projectile aim where we click
+   const angle = Math.atan2(
+       event.clientY - canvas.height / 2,
+       event.clientX - canvas.width / 2,
+       
+    )
+
+    const velocity = {
+        x: Math.cos(angle), //returns angles between -1 to 1
+        y: Math.sin(angle)
+    }
+
+   projectiles.push(new Projectile(
+        canvas.width / 2, canvas.height / 2, 5, 'red',
+        velocity
+       ))
 })
+
+animate()
