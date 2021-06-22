a834602d6ce2da2172698fa6618be89a85ab23b9
Hunnser86 <robhunns@gmail.com>
2021-06-22 14:02:07 +0000

    Created enemy class and added it to the animate function

--- assets/js/index.js
+++ assets/js/index.js
@@ -53,32 +53,75 @@ class Projectile {
      }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color  
+        this.velocity = velocity 
+    }
+
+    draw() {
+        ctxt.beginPath()
+        ctxt.arc(
+            this.x, this.y, this.radius, 0, Math.PI * 2,
+            false) 
+        ctxt.fillStyle = this.color   
+        ctxt.fill()    
+     }
+ 
+     update() {
+         this.draw()
+         this.x = this.x + this.velocity.x
+         this.y = this.y + this.velocity.y
+     }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 
 //Create the player
 const player = new Player(x, y, 30, 'blue' )
-player.draw()
+
 
 //Put the projectile here to allow it be accessed within the animate function
 const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {
    x: 1,
    y: 1
 })
-
-//create an array to draw multiple projectiles
+//create an array to draw multiple projectiles and enemies
 const projectiles = []
+const enemies = []
 
-
+function spawnEnemies() {
+    setInterval( ()=> {
+       const x = 100
+       const y = 100 
+       const radius = 30
+       const color = 'green'
+       const velocity = {
+           x: 1,
+           y: 1
+       }
+       enemies.push(new Enemy(x, y, radius, color, velocity))
+       console.log(enemies)
+    }, 1000)
+}
 
 //Create a function to animate the projectile
 function animate() {
     requestAnimationFrame(animate)
+    ctxt.clearRect(0, 0, canvas.width, canvas.height)//cleard the screen to show circle projectiles rather than lines
+    player.draw()
     projectiles.forEach(projectile => {
         projectile.update()
-    });
-    
+    })
+
+    enemies.forEach(enemy => {
+        enemy.update()
+    })   
 }
 
 
@@ -104,3 +147,4 @@ addEventListener('click', (event)=>
 })
 
 animate()
+spawnEnemies()
