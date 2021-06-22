4d8490134384c4d6be6acb0daecfb384ff84f8fe
Hunnser86 <robhunns@gmail.com>
2021-06-22 12:47:00 +0000

    Created the Projectile class and projectile object.  Enabled the projectile to be drawn to canvas when there is a click event

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,13 +21,44 @@ class  Player {
        ctxt.arc(
            this.x, this.y, this.radius, 0, Math.PI * 2,
            false) 
+       ctxt.fillStyle = this.color   
        ctxt.fill()    
     }
 
 }
 
+//Create the projectiles
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color  
+        this.velocity = velocity 
+    }
+
+    draw() {
+        ctxt.beginPath()
+        ctxt.arc(
+            this.x, this.y, this.radius, 0, Math.PI * 2,
+            false) 
+        ctxt.fillStyle = this.color   
+        ctxt.fill()    
+     }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+
 //Create the player
-const player = new Player(100, 100, 30, 'blue' )
+const player = new Player(x, y, 30, 'blue' )
 player.draw()
 
 console.log(player)
+
+//Add an event listener to listen for a click event which will create a projectile
+addEventListener('click', (event)=>{
+    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
+    projectile.draw()
+})
